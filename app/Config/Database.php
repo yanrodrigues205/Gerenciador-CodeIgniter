73538e870831f14922ea3d4130a90f4b3cc5fc29
yanrodrigues205 <yan.pablo205@gmail.com>
2024-01-10 11:09:49 -0300

    :bug: fix: setting database name

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'amzmp',
+        'database'     => 'software',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
